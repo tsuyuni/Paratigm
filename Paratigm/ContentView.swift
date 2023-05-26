d191303fc499948d104b54e05683c5cd9b5308a1
梅雨 <yuri.tsuchikawa@gmail.com>
2023-05-27 01:04:51 +0900

    Add Play/Pause buttons

--- Paratigm/ContentView.swift
+++ Paratigm/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+enum ContentState {
+    case first
+    case second
+}
+
 struct ContentView: View {
+    
+    @State var statusArray: [ContentState] = [.first, .first, .first, .first, .first]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            
+            ForEach (0 ..< statusArray.count, id: \.self) { index in
+                HStack {
+                    switch statusArray[index] {
+                    case .first:
+                        PlayView(state: $statusArray[index])
+                    case .second:
+                        PauseView(state: $statusArray[index])
+                    }
+                    Text("0")
+                    Spacer()
+                }
+                .contentShape(Rectangle())
+                .accessibilityIdentifier("content")
+            }
         }
-        .padding()
+    }
+}
+
+struct PlayView: View {
+    @Binding var state: ContentState
+    var body: some View {
+        Image(systemName: "play.fill")
+            .resizable()
+            .renderingMode(.template)
+            .foregroundColor(.green)
+            .frame(width: 30.0, height: 30.0)
+            .onTapGesture {
+                state = .second
+                print(state)
+            }
+    }
+}
+
+struct PauseView: View {
+    @Binding var state: ContentState
+    var body: some View {
+        Image(systemName: "pause.fill")
+            .resizable()
+            .renderingMode(.template)
+            .foregroundColor(.green)
+            .frame(width: 30.0, height: 30.0)
+            .onTapGesture {
+                state = .first
+                print(state)
+            }
     }
 }
 
