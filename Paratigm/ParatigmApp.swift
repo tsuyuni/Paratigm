f7d49c2db117ceb3387e721986e7e9048ee538c6
梅雨 <yuri.tsuchikawa@gmail.com>
2023-05-26 20:58:41 +0900

    Initial Commit

---
+++ Paratigm/ParatigmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParatigmApp.swift
+//  Paratigm
+//
+//  Created by Tsuchikawa Yuri on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ParatigmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
